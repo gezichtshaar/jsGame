96670c065af5009ec811357590c0e44ed6ca32f8
gezichtshaar <tedteddet@gmail.com>
2014-03-02 22:32:44 +0100

    schaal werkend

--- Game.js
+++ Game.js
@@ -6,6 +6,8 @@ GameManager = function (_location, _width, _height) {
 		running = false,
 		graphicsManager, inputManager;
 
+	requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+
 	function render () {
 		graphicsManager.clear();
 		gamestate[gamestate.length - 1].render(graphicsManager);
@@ -19,6 +21,7 @@ GameManager = function (_location, _width, _height) {
 		if (running) {
 			render();
 			update(1);
+			requestAnimationFrame(tick);
 		}
 	}
 
@@ -42,12 +45,33 @@ GraphicsManager = function (_width, _height) {
 		height = _height,
 		canvas, context;
 
-	canvas = document.createElement("canvas");
-	canvas.width = width;
-	canvas.height = height;
+	init = function () {
+		canvas = document.createElement("canvas");
+		canvas.width = width;
+		canvas.height = height;
+		canvas.className = "gamescreen";
 
-	context = canvas.getContext("2d");
+		context = canvas.getContext("2d");
+		updateContext();
+		window.addEventListener("resize", resizeCanvas);
+	}
+	updateCanvas = function () {
+		canvas.width = width;
+		canvas.height = height;
+	}
+	updateContext = function () {
+		context.imageSmoothingEnabled = false;
+        context.webkitImageSmoothingEnabled = false;
+        context.mozImageSmoothingEnabled = false;
+	}
+	resizeCanvas = function (_event) {
+		width = canvas.parentElement.clientWidth;
+		height = canvas.parentElement.clientHeight;
+		updateCanvas();
+		updateContext();
+	}
 
+	init();
 	return {
 		canvas: canvas,
 		context: context,
@@ -59,7 +83,14 @@ GraphicsManager = function (_width, _height) {
 			context.drawImage(Content[image], dx/100 * width, dy/100 * height);
 		},
 		drawText: function (text, dx, dy) {
-			
+			var fontmap = " !\"#$%&'()*+,-./0123456789:;<=>?"+
+			              "@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_"+
+			              "`abcdefghijklmnopqrstuvwxyz{|}~ ";
+
+			for (var n = 0; n < text.length; n++) {
+				character = fontmap.indexOf(text[n]);
+				context.drawImage(Content["font"], (character % 32) * 4, (character >> 5) * 8, 4, 8, Math.floor(dx/100 * width) + n * 16, Math.floor(dy/100 * height), 16, 32);
+			}
 		}
 	};
 }
@@ -85,7 +116,7 @@ MenuScreen = function () {
 
 	init = function () {
 		buttons.push(new Button("Start", 50, 50, startGame));
-		buttons.push(new Button("bam", 50, 60, startGame));
+		buttons.push(new Button("Options", 50, 60, startGame));
 	}
 	startGame = function () {
 		console.log("start");
@@ -119,15 +150,14 @@ Button = function (_text, _top, _left, _callback) {
 			}
 		},
 		render: function (graphics) {
-			graphics.drawImage("button1", top, left);
+			graphics.drawText(text, top, left);
 		}
 	}
 }
 
 Content = (function () {
 	var images = {"header": "resources/images/header.png",
-				  "button1": "resources/images/button1.png",
-				  "button2": "resources/images/button2.png"},
+				  "font": "resources/fonts/font.png"},
 		content = {};
 
 	addImages = function (resources) {
@@ -157,6 +187,6 @@ Game = function () {
 }
 
 loader.push(function() {
-	var gameManager = new GameManager(document.body, 500, 500);
+	var gameManager = new GameManager(document.body, document.body.clientWidth, document.body.clientHeight);
 	gameManager.init();
 });
